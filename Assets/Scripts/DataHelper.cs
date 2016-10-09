9be5dcdeffbe4e08a216049862a9d9d1b379765e
isnainul <isnainul.fahrizal@student.uny.ac.id>
2016-10-09 13:25:43 +0700

    MenuScene

---
+++ Assets/Scripts/DataHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataHelper : MonoBehaviour {
+	public static void setVRMode(bool vrMode) {
+		PlayerPrefs.SetInt ("VRMode", vrMode ? 1 : 0);
+	}
+
+	public static bool getVRMode() {
+		return PlayerPrefs.GetInt ("VRMode",0) == 1 ? true : false;
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	const string PLAY_SCENE_NAME = "PlayScene";
+
+	GvrViewer gvrViewer;
+
+	void Start () {
+		gvrViewer = GvrViewer.Instance;
+		if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name.Equals (PLAY_SCENE_NAME)) {
+			gvrViewer.VRModeEnabled = DataHelper.getVRMode ();
+		}
+	}
+	
+	public void Play(bool vrMode) {
+		DataHelper.setVRMode (vrMode);
+
+		SceneManager.LoadSceneAsync (PLAY_SCENE_NAME);
+	}
+}
